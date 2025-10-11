c042822270f511fa133b03a2f5c4651753db92a5
itzmeowww <winthanasan@gmail.com>
2025-10-11 10:53:31 +0900

    feat: navigation

---
+++ ios/escape/escape/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  escape
+//
+//  Created by Thanasan Kumdee on 11/10/2568 BE.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ ios/escape/escape/Navigation/NavigationView.swift
@@ -0,0 +1,33 @@
+//
+//  NavigationView.swift
+//  escape
+//
+//  Created by Thanasan Kumdee on 11/10/2568 BE.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+
+            MapView()
+                .tabItem {
+                    Label("Map", systemImage: "map.fill")
+                }
+
+            SettingView()
+                .tabItem {
+                    Label("Setting", systemImage: "gearshape.fill")
+                }
+        }
+    }
+}
+
+#Preview {
+    NavigationView()
+}
