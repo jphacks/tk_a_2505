72f51c908c9bb4f3be513de4c127e931f6cd85b8
Cerealmaster0621 <cerealmaster@naver.com>
2025-10-11 15:36:05 +0900

    added point, user model

---
+++ ios/escape/escape/Models/UserModel.swift
@@ -0,0 +1,34 @@
+//
+//  UserModel.swift
+//  escape
+//
+//  Created by YoungJune Kang on 2025/10/11.
+//
+
+import Foundation
+
+struct User: Codable, Identifiable {
+    let id: UUID
+    let createdAt: Date
+    let name: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case createdAt = "created_at"
+        case name
+    }
+}
+
+// MARK: - Helper Extensions
+
+extension User {
+    /// Returns the display name, or a default if name is nil
+    var displayName: String {
+        name ?? "Anonymous User"
+    }
+
+    /// Returns true if the user has set their name
+    var hasName: Bool {
+        name != nil && !(name?.isEmpty ?? true)
+    }
+}
