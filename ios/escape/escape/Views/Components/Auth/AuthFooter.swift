ddfb83e0f9f9c62856dd7316c6a0373a5faa2b04
Thanasan Kumdee <winthanasan@gmail.com>
2025-10-27 20:20:22 +0900

    fix link

--- ios/escape/escape/Views/Components/Auth/AuthFooter.swift
+++ ios/escape/escape/Views/Components/Auth/AuthFooter.swift
@@ -18,7 +18,7 @@ struct AuthFooter: View {
 
             HStack(spacing: 3) {
                 Button(action: {
-                    if let url = URL(string: "https://jphacks.github.io/tk_a_2505/terms-of-service") {
+                    if let url = URL(string: "https://jphacks.github.io/tk_a_2505/tos") {
                         openURL(url)
                     }
                 }) {
@@ -32,7 +32,7 @@ struct AuthFooter: View {
                     .foregroundColor(.secondary.opacity(0.8))
 
                 Button(action: {
-                    if let url = URL(string: "https://jphacks.github.io/tk_a_2505/privacy-policy") {
+                    if let url = URL(string: "https://jphacks.github.io/tk_a_2505/privacy") {
                         openURL(url)
                     }
                 }) {
